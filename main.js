411012055a8117f2ea3bac3a53aa8afbc6145f79
Andrzej Łuszcz <endriu1297@gmail.com>
2025-08-08 08:56:39 +0200

    feat: add showing data from data.json

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
 const filters = document.querySelectorAll('.profile__filter');
+let activeFilter = 'weekly';
+let data = [];
+const labels = {
+  daily: 'Yesterday',
+  weekly: 'Last Week',
+  monthly: 'Last Month',
+};
 
-[...filters].forEach((filter) => {
+function updateDashboardData() {
+  for (const profile of data) {
+    const panel = document.querySelector(`[data-name="${profile.title}"]`);
+    const previousTime = panel.querySelector('.tracker__last');
+    const currentTime = panel.querySelector('.tracker__time');
+    const timeframe = profile.timeframes[activeFilter];
+
+    previousTime.textContent = `${labels[activeFilter]} - ${profile.timeframes.daily.previous}hrs`;
+    currentTime.textContent = `${profile.timeframes.daily.current}hrs`;
+  }
+}
+
+for (const filter of filters) {
   filter.addEventListener('click', () => {
     filters.forEach((f) => f.classList.remove('profile__filter--active'));
     filter.classList.add('profile__filter--active');
+    activeFilter = filter.textContent.toLowerCase();
+
+    updateDashboardData();
   });
+}
+
+window.addEventListener('load', async () => {
+  try {
+    const response = await fetch('./data.json');
+    data = await response.json();
+    updateDashboardData();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 });
